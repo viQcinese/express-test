f792428bbb96383eadb82f943429406137010b4d
Vitor Senise <vitor.senise@gmail.com>
2024-12-22 19:34:48 -0300

    Listens to messages

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/", (req, res) => {
   res.send(process.env.MSG);
 });
 
+app.post("/", (req, res) => {
+  console.log(req.body);
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
